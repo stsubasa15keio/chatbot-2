fa543c0bd87a1e2dce698f06af361ba3f6f21ddd
stsubasa15keio <s.tsubasa15@keio.jp>
2025-01-06 22:03:14 +0900

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,7 +1,12 @@
 import os
 from dotenv import load_dotenv
 
+# .envファイルの読み込み
 load_dotenv()
 
+# LINE Botの設定
 CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN')
 CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
+
+# OpenAI APIの設定
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
